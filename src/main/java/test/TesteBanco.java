580849f47b45051f913850f508f525cb50ee700c
Janio <janiojunior@gmail.com>
2022-03-26 11:20:35 -0300

    Teste de conexao com o banco

---
+++ src/main/java/test/TesteBanco.java
@@ -0,0 +1,74 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TesteBanco {
+
+	public static void main(String[] args) {
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Driver não encontrado. Faça o download.");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/livrosdb", 
+					"topicos1", "123456");
+		} catch (SQLException e) {
+			System.out.println("Problema ao conectar no banco de dados. Verifique as informacoes de conexao.");
+			e.printStackTrace();
+		}
+		StringBuffer sql = new StringBuffer();
+		sql.append("SELECT ");
+		sql.append("  u.id, ");
+		sql.append("  u.nome, ");
+		sql.append("  u.login, ");
+		sql.append("  u.senha ");
+		sql.append("FROM ");
+		sql.append("  usuario u ");
+		sql.append("ORDER BY ");
+		sql.append("  u.nome DESC, u.login DESC ");
+		
+		ResultSet rs = null;
+		try {
+			rs = conn.createStatement().executeQuery(sql.toString());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			while (rs.next()) {
+				System.out.println(rs.getInt("id"));
+				System.out.println(rs.getString("nome"));
+				System.out.println(rs.getString("login"));
+				System.out.println(rs.getString("senha"));
+				System.out.println("");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			rs.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
